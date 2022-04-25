a77c1b49b08768280c0b6eec738f28a86d6a9645
Eduardo <eduardosouzv@gmail.com>
2022-04-24 21:31:17 -0300

    order asc by default

--- src/graphql/modules/tweet/resolvers.ts
+++ src/graphql/modules/tweet/resolvers.ts
@@ -7,7 +7,7 @@ interface ITweet {
 
 export default {
   Query: {
-    tweets: async () => await Tweet.find(),
+    tweets: async () => await Tweet.find().sort({ _id: -1 }),
     tweetById: async (_: unknown, { id }: { id: string }) =>
       await Tweet.findById(id),
   },
