2f9786f4aa9b6b7f081e8da6656346338ad1e4cf
Eduardo <eduardosouzv@gmail.com>
2022-05-10 19:48:33 -0300

    test: currentuser / login

--- __tests__/User.test.ts
+++ __tests__/User.test.ts
@@ -1,3 +1,4 @@
+import faker from '@faker-js/faker';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import server from '../src/app';
@@ -63,5 +64,81 @@ describe('User', () => {
     expect(response.body.errors).toBeDefined();
     expect(response.body.errors?.length).toBeGreaterThan(0);
     expect(response.status).toBe(200);
+  });
+
+  it('should login with a user', async () => {
+    const user = faker.name.firstName();
+    await request('http://localhost:4000/')
+      .post('/')
+      .send({
+        query: `
+        mutation User {
+          registerUser(username: "${user}", password: "strong@pass") {
+            token
+          }
+        }`,
+      });
+
+    const response = await request('http://localhost:4000/')
+      .post('/')
+      .send({
+        query: `
+        query {
+          loginUser(username:"${user}" , password: "strong@pass") {
+            token
+            user {
+              id
+              name
+            }
+          }
+        }`,
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.loginUser).toEqual(
+      expect.objectContaining({
+        token: expect.any(String),
+        user: {
+          id: expect.any(String),
+          name: user,
+        },
+      })
+    );
+  });
+
+  it('should create a user and return in getCurrentUser route', async () => {
+    const user = faker.name.firstName();
+    const userRequest = await request('http://localhost:4000/')
+      .post('/')
+      .send({
+        query: `
+        mutation User {
+          registerUser(username: "${user}", password: "strong@pass") {
+            token
+          }
+        }`,
+      });
+    const { token } = userRequest.body.data.registerUser;
+
+    const response = await request('http://localhost:4000/')
+      .post('/')
+      .set('Authorization', token)
+      .send({
+        query: `
+        query {
+          getCurrentUser {
+            id
+            name
+          }
+        }`,
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.data.getCurrentUser).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: user,
+      })
+    );
   });
 });
